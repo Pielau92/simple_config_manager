6f064613977575682f2e33809f8b5c8d064757f5
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-10-01 10:44:51 +0200

    Tidy up configs.py

--- src/simple_config_manager/configs.py
+++ src/simple_config_manager/configs.py
@@ -1,37 +1,27 @@
-from os.path import join, expanduser
 from dataclasses import dataclass, fields
 from typing import Type, TypeVar, List
 
 import configparser
 
-import sections
-
 T = TypeVar("T")  # type placeholder
 
 
 @dataclass
-class Configs:
-    """Dataclass for storing configurations.
+class _Configs:
+    """Parent dataclass for creating user-defined configuration classes through inheritance.
 
-    Each field within this dataclass contains a dataclass defined in sections.py. Each of those dataclasses contains
-    static (imported from .ini file) and/or runtime (set automatically at runtime) configurations."""
+    A configuration class is used to read and store configuration values from an .ini file. See example.py for more
+    information on how a configuration class can be defined."""
 
-    general: sections.General
-    filenames: sections.Filenames
-    sheetnames: sections.SheetNames
-    col_headers: sections.ColumnHeaders
-    time: sections.Time
-    runtime: sections.Runtime = None
+    def __init__(self, path_ini: str = None):
+        """Initialize configuration object by loading configurations from an ini file.
 
-    def __init__(self, path: str):
-        """Initialize Configs by loading all configurations from an ini file.
-
-        :param str path: path to configurations ini file
+        :param str path_ini: path to configurations .ini file
         """
 
         for field in fields(self):
             if field.default is not None:  # if default value 'None' was set, do not try to load section from ini file
-                setattr(self, field.name, self._get_ini_section(path, field.type))
+                setattr(self, field.name, self._get_ini_section(path_ini, field.type))
 
     @staticmethod
     def _get_ini_section(path: str, cls: Type[T]) -> T:
@@ -90,74 +80,3 @@ class Configs:
             kwargs[name] = value
 
         return cls(**kwargs)
-
-
-@dataclass
-class Paths:
-    _configs: Configs
-    root: str  # path to root directory
-    config: str  # path to configuration ini file
-    original_sim_variants_excel: str  # path to original simulation variants Excel file
-
-    results_dir: str = join(expanduser('~'), 'documents', 'TRNSYSAuto')  # path to results output directory
-
-    @property
-    def configs(self) -> str:
-        """Path to configs.ini file."""
-        return join(self.root, 'configs.ini')
-
-    @property
-    def sim_series_dir(self) -> str:
-        """Path to simulation series directory."""
-        return join(self.results_dir, self._configs.runtime.dirname_sim_series)
-
-    @property
-    def logfile(self) -> str:
-        """Path to logfile."""
-        return join(self.sim_series_dir, self._configs.filenames.logger)
-
-    @property
-    def savefile(self):
-        """Path to savefile where the SimulationSeries object (and the simulation/evaluation progress) is saved."""
-        return join(self.sim_series_dir, self._configs.filenames.savefile)
-
-    @property
-    def data_dir(self):
-        """Path to data directory (contains input directory and results directory)."""
-        return join(self.root, 'data')
-
-    @property
-    def input_dir(self):
-        """Path to input directory (optional storage location for simulation variants Excel files, default initial
-        directory when asking to select a simulation variants Excel file)."""
-        return join(self.data_dir, 'input')
-
-    @property
-    def assets_dir(self):
-        """Path to assets directory (contains all files directly needed by TRNSYS)."""
-        return join(self.root, 'assets')
-
-    @property
-    def sim_variants_excel(self):
-        """Path to simulation series Excel file copy."""
-        return join(self.sim_series_dir, self._configs.runtime.filename_sim_variants_excel) + '.xlsx'
-
-    @property
-    def evaluation_save_dir(self):
-        """Path to directory, where evaluation results are saved."""
-        return join(self.sim_series_dir, 'evaluation')
-
-    @property
-    def cumulative_evaluation_save_file(self):
-        """Path to cumulative evaluation file."""
-        return join(self.evaluation_save_dir, 'gesamt.xlsx')
-
-    @property
-    def cumulative_evaluation_template(self):
-        """Path to cumulative evaluation template file."""
-        return join(self.assets_dir, 'Auswertung_Gesamt.xlsx')
-
-    @property
-    def variant_evaluation_template(self):
-        """Path to variant evaluation template file."""
-        return join(self.assets_dir, 'Auswertung_Variante.xlsx')
